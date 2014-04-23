a6f39b3657ff0875c2234d180684b6666270068d
Martin Hulten-Ashauer <martin@mortos.org>
2014-04-24 09:54:30 +1000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+from setuptools import setup
+
+setup(
+    name = 'tagm-open',
+    version = '0.1-dev',
+
+    maintainer = u'Martin Hult\xe9n-Ashauer',
+    maintainer_email = 'tagm@nimdraug.com',
+    url = 'http://github.com/Nimdraug/tagm-open',
+    license = 'MIT',
+    description = 'Command tool to open files based on it\'s tagm tags.',
+
+    py_modules = [
+        'tagm_open'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'tagm-open = tagm_open:main'
+        ]
+    }
+)
