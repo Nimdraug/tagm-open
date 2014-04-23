09ffb564b77baafe057378b286299edb46368fb4
Martin Hulten-Ashauer <martin@mortos.org>
2014-04-24 09:50:31 +1000

    prepared for setup.py

--- tagm-open.py
+++ tagm-open.py
@@ -1,45 +1,50 @@
+#!/usr/bin/env python2
 import ConfigParser, sys, os.path, argparse
 from tagm import (
     TagmDB, find_db, process_paths, parse_tagpaths
 )
 from subprocess import call
 
-# Setup argparser
-parser = argparse.ArgumentParser()
-parser.add_argument( '--tags', action = 'store_true' )
-parser.add_argument( '--conf', action = 'store', default = '~/.config/tagm-open.conf' )
-parser.add_argument( 'files_or_tags', nargs = '+' )
+def main():
+    # Setup argparser
+    parser = argparse.ArgumentParser()
+    parser.add_argument( '--tags', action = 'store_true' )
+    parser.add_argument( '--conf', action = 'store', default = '~/.config/tagm-open.conf' )
+    parser.add_argument( 'files_or_tags', nargs = '+' )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-# Load config
-conf = ConfigParser.ConfigParser()
+    # Load config
+    conf = ConfigParser.ConfigParser()
 
-conf.read( os.path.expanduser( args.conf ) )
+    conf.read( os.path.expanduser( args.conf ) )
 
-mimes = []
-tagmatch = []
+    mimes = []
+    tagmatch = []
 
-for sect in conf.sections():
-    opts = dict( conf.items( sect ) )
-    if 'mime' in opts:
-        mimes.append( ( opts['mime'], sect ) )
-    if 'tags' in opts:
-        tags = parse_tagpaths( opts['tags'].split( ',' ) )
-        tagmatch.append( ( tags, sect ) )
+    for sect in conf.sections():
+        opts = dict( conf.items( sect ) )
+        if 'mime' in opts:
+            mimes.append( ( opts['mime'], sect ) )
+        if 'tags' in opts:
+            tags = parse_tagpaths( opts['tags'].split( ',' ) )
+            tagmatch.append( ( tags, sect ) )
 
-dbpath = find_db()
-db = TagmDB( os.path.join( dbpath, '.tagm.db' ) )
+    dbpath = find_db()
+    db = TagmDB( os.path.join( dbpath, '.tagm.db' ) )
 
-# Match file(s)
-for f in process_paths( dbpath, args.files_or_tags ):
-    tags = db.get_obj_tags( [ f ] )
-    for mtags, sect in tagmatch:
-        matches = filter( lambda tag: tag in mtags, tags )
-        if matches == mtags:
+    # Match file(s)
+    for f in process_paths( dbpath, args.files_or_tags ):
+        tags = db.get_obj_tags( [ f ] )
+        for mtags, sect in tagmatch:
+            matches = filter( lambda tag: tag in mtags, tags )
+            if matches == mtags:
 
-            opts = dict( conf.items( sect ) )
-            command = opts['command'].replace( '%n', f )
-            call( command, shell = True )
+                opts = dict( conf.items( sect ) )
+                command = opts['command'].replace( '%n', f )
+                call( command, shell = True )
 
-            break
+                break
+
+if __name__ == '__main__':
+    main()
